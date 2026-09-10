352b4707348d66c50b8335a378d3f1d94f372626
rmaxx02 <rmaxx2003@gmail.com>
2026-09-10 14:24:01 -0400

    Fix capitalization in target year tag name

--- bot.py
+++ bot.py
@@ -182,7 +182,7 @@ async def on_message(msg):
                     thread_date_prefix = dt_obj.strftime("%b %Y")
                     
                     if dt_obj.year == datetime.now().year:
-                        target_year_tag_name = f"{dt_obj.year} current"
+                        target_year_tag_name = f"{dt_obj.year} Current"
                     else:
                         target_year_tag_name = f"{dt_obj.year} Archive"
         except Exception as e:
